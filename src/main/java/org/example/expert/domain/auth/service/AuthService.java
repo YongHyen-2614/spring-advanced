15c4428d2997a09e9ab8b68bdfd19aef53460610
YongHyen-2614 <jkljkl0614@gmail.com>
2025-08-26 04:06:36 +0900

    레벨 1-1: Early Return

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -25,23 +25,27 @@ public class AuthService {
 
     @Transactional
     public SignupResponse signup(SignupRequest signupRequest) {
-
-        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
-
-        UserRole userRole = UserRole.of(signupRequest.getUserRole());
-
+        // 중복 검사
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
             throw new InvalidRequestException("이미 존재하는 이메일입니다.");
         }
-
+        // 비밀번호 인코딩
+        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+        // UserRole 변환
+        UserRole userRole = UserRole.of(signupRequest.getUserRole());
+        // 저장
         User newUser = new User(
                 signupRequest.getEmail(),
                 encodedPassword,
                 userRole
         );
         User savedUser = userRepository.save(newUser);
-
-        String bearerToken = jwtUtil.createToken(savedUser.getId(), savedUser.getEmail(), userRole);
+        // 토큰
+        String bearerToken = jwtUtil.createToken(
+                savedUser.getId(),
+                savedUser.getEmail(),
+                userRole
+        );
 
         return new SignupResponse(bearerToken);
     }
