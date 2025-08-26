56997e3a5a7562a4a4b3c600306ba951924d7aa2
YongHyen-2614 <jkljkl0614@gmail.com>
2025-08-26 10:13:38 +0900

    fix: 레벨 1-3: Validation

--- src/main/java/org/example/expert/domain/user/controller/UserController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserController.java
@@ -2,8 +2,6 @@ package org.example.expert.domain.user.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.common.annotation.Auth;
-import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.user.dto.request.UserChangePasswordRequest;
 import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.service.UserService;
@@ -19,11 +17,6 @@ public class UserController {
     @GetMapping("/users/{userId}")
     public ResponseEntity<UserResponse> getUser(@PathVariable long userId) {
         return ResponseEntity.ok(userService.getUser(userId));
-    }
-
-    @PutMapping("/users")
-    public void changePassword(@Auth AuthUser authUser, @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
-        userService.changePassword(authUser.getId(), userChangePasswordRequest);
     }
 
     @PostMapping("/password")
